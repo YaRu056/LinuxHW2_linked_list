2711eb142e5f56d17a323dde5712e0485083d8f0
YaRu056 <yaru1010514@gmail.com>
2024-03-13 00:11:11 +0800

    Complete linux list API quick_sort
    I add rand_pivot() to choose random pivot to avoid the worst case in
    quicksort.

--- quick_sort/qsort.c
+++ quick_sort/qsort.c
@@ -3,74 +3,129 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "list.h"
+#include <time.h>
 /* Verify if list is order */
 typedef struct __node {
-    struct __node *left, *right;
-    struct __node *next;
+
     long value;
+    struct list_head list;
 } node_t;
 
-void list_add(node_t **list, node_t *node_t)
+struct list_head *list_new()
 {
-    node_t->next = *list;
-    *list = node_t;
-}
-
-node_t *list_tail(node_t **left)
-{
-    while ((*left) && (*left)->next)
-        left = &((*left)->next);
-    return *left;
-}
-
-int list_length(node_t **left)
-{
-    int n = 0;
-    while (*left) {
-        ++n;
-        left = &((*left)->next);
+    struct list_head *head = (struct list_head *)malloc(sizeof(struct list_head));
+    if (!head) {
+        //printf("Fail to allocate space to an empty queue!\n");
+        return NULL;
     }
-    return n;
+    INIT_LIST_HEAD(head);
+    return head;
 }
-
-node_t *list_construct(node_t *list, int n)
+/* Free all storage used by list */
+void list_free(struct list_head *head)
 {
-    node_t *node = malloc(sizeof(node_t));
-    node->next = list;
+    if (!head)
+        return;
+    node_t *entry, *safe;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        free(entry);
+    }
+    free(head);
+    head = NULL;
+    return;
+}
+/* Insert an element at head of list */
+bool list_insert_head(struct list_head *head, int n)
+{
+    if (!head )
+        return false;
+    node_t *node = (node_t*) malloc(sizeof(node_t));
+    if (!node) {
+        return false;
+    }
+    INIT_LIST_HEAD(&node->list);
     node->value = n;
-    return node;
-}
-
-void list_free(node_t **list)
-{
-    node_t *node = (*list)->next;
-    while (*list) {
-        free(*list);
-        *list = node;
-        if (node)
-            node = node->next;
+    if (!node->value) {
+        // printf("Fail to allocate space to string!\n");
+        free(node);
+        return false;
     }
-}
-
-static bool list_is_ordered(node_t *list)
-{       
-    bool first = true;
-    int value;
-    while (list) {
-        if (first) {
-            value = list->value;
-            first = false;
-        } else {
-            if (list->value < value)
-                return false;
-            value = list->value;
-        }
-        list = list->next;
-    }
+    //printf("Insert\n");
+    list_add(&node->list, head);
     return true;
 }
 
+int list_length(struct list_head *head)
+{
+    if (!head || list_empty(head)) {
+        return 0;
+    }
+    int len = 0;
+    struct list_head *tmp;
+    list_for_each (tmp, head) {
+        len++;
+    }
+    return len;
+}
+
+static bool list_is_ordered(struct list_head *head)
+{       
+    if (!head || list_empty(head)) {
+        return true;
+    }
+    struct list_head *cur;
+    bool first = true;
+    long value;
+    list_for_each(cur,head){
+        if (cur->next == head){
+            break;
+        }
+        node_t *p = list_entry(cur, node_t, list);
+        node_t *n = list_entry(cur->next, node_t, list);
+        if (p ->value > n->value) {
+            return false;
+        } 
+    }
+    return true;
+}
+void n_swap(struct list_head *node1, struct list_head *node2)
+{
+    struct list_head *tmp = node1->next;
+    node1->next = node2->next;
+    node2->next = tmp;
+    if (node1->next != NULL) {
+        node1->next->prev = node1;
+    }
+    if (node2->next != NULL) {
+        node2->next->prev = node2;
+    }
+    tmp = node1->prev;
+    node1->prev = node2->prev;
+    node2->prev = tmp;
+    if (node1->prev != NULL) {
+        node1->prev->next = node1;
+    }
+    if (node2->prev != NULL) {
+        node2->prev->next = node2;
+    }
+}
+
+void rand_pivot(struct list_head *head)
+{
+    if (!head || list_empty(head) ||  list_is_singular(head)) {
+        return;
+    }
+    int n = rand() % list_length(head);
+    struct list_head *cur;
+    list_for_each (cur, head) {
+        if (n == 0)
+            break;
+        n--;
+    }
+    n_swap(head->next, cur);
+
+}
 /* shuffle array, only work if n < RAND_MAX */
 void shuffle(int *array, size_t n)
 {
@@ -84,71 +139,98 @@ void shuffle(int *array, size_t n)
         array[i] = t;
     }
 }
+node_t *list_remove_head(struct list_head *head)
+{
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    node_t *node;
+    node = list_first_entry(head, node_t, list);
+    if (node == NULL) {
+        free(node);
+        return NULL;
+    }
+    list_del(&node->list);
+    return node;
+}
+void quick_sort(struct list_head **head)
+{
+    if (!(*head) || list_empty(*head) ||  list_is_singular(*head)) {
+        return;
+    }
+    int n = list_length(*head);
+    int value;
+    int i = 0;
+    int max_level = 2 * n;
+    struct list_head *begin[max_level];
+    struct list_head *result = list_new(), *left = list_new(), *right = list_new();
+    
+    begin[0] = *head;
+    for(int j=1;j<max_level;j++){
+        begin[j] = list_new();
+    }           
+    while (i >= 0) {
+        struct list_head *L = begin[i]->next, *R = begin[i]->prev;
+        if (L != R) {
+            rand_pivot(begin[i]);
+            node_t *pivot = list_remove_head(begin[i]);
+            node_t *entry, *safe;
+            list_for_each_entry_safe (entry, safe, begin[i], list) {
+                list_del(&entry->list);//delete entry->list relation
+                if (entry->value > pivot->value) {
+                    //printf("add right\n");
+                    list_add(&entry->list, right);
+                } else {
+                    //printf("add left\n");
+                    list_add(&entry->list, left);
+                }
+            }
+            list_splice_init(left, begin[i]);
+            //printf("add pivot\n");
+            list_add(&pivot->list, begin[i + 1]);
+            list_splice_init(right, begin[i + 2]);
+            i += 2;
+        } else {
+            if (list_is_singular(begin[i]))
+                list_splice_init(begin[i], result);
+            i--;
+        }
+    }
+    *head = result;
+    for (int i = 0; i < max_level; i++)
+        list_free(begin[i]);
+    list_free(left);
+    list_free(right);
+
+}
 
 int main(int argc, char **argv)
 {
-    node_t *list = NULL;
+    struct list_head *list = list_new();
 
     size_t count = 100000;
 
-    int *test_arr = malloc(sizeof(int) * count);
+    int *test_arr = (int *)malloc(sizeof(int) * count);
 
     for (int i = 0; i < count; ++i)
         test_arr[i] = i;
     shuffle(test_arr, count);
 
     while (count--)
-        list = list_construct(list, test_arr[count]);
+        list_insert_head(list, test_arr[count]);
 
     quick_sort(&list);
     assert(list_is_ordered(list));
-
-    list_free(&list);
+    /*
+    //print sorted list
+    node_t *cur;
+    list_for_each_entry (cur, list, list){
+        printf("%ld ", cur->value);
+    }
+    printf("\n");*/
+    list_free(list);
 
     free(test_arr);
 
-    return;
-}
-void quick_sort(node_t **list)
-{
-    int n = list_length(list);
-    int value;
-    int i = 0;
-    int max_level = 2 * n;
-    node_t *begin[max_level], *end[max_level];
-    node_t *result = NULL, *left = NULL, *right = NULL;
-    
-    begin[0] = *list;
-    end[0] = list_tail(list);
-            
-    while (i >= 0) {
-        node_t *L = begin[i], *R = end[i];
-        if (L != R) {
-            node_t *pivot = L;
-            value = pivot->value;
-            node_t *p = pivot->next;
-            pivot->next = NULL;
-    
-            while (p) {
-                node_t *n = p;
-                p = p->next;
-                list_add(n->value > value ? &right : &left, n);
-            }
-
-            begin[i] = left;
-            end[i] = list_tail(&left);
-            begin[i + 1] = pivot;
-            end[i + 1] = pivot;
-            begin[i + 2] = right;
-            end[i + 2] = list_tail(&right);
-
-            left = right = NULL;
-            i += 2;
-        } else {
-            if (L)
-                list_add(&result, L);
-            i--;
-        }
-    }
-    *list = result;
+    return 0;
 }
